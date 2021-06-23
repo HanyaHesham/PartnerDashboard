4a19f096087097643f993356cb6e40c073c77cee
MariamShalaby <77920984+MariamShalaby11@users.noreply.github.com>
2021-06-24 01:48:22 +0200

    Update AppRouter.js

--- src/components/Router/AppRouter.js
+++ src/components/Router/AppRouter.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import{BrowserRouter as Router,Redirect,Route} from 'react-router-dom';
-import Slidebar from '../Slidebar';
+import{BrowserRouter as Router,Route} from 'react-router-dom';
+//import Slidebar from '../Slidebar';
 import Restaurant from './../Restaurant/Restaurant'
 import AddMeal from './../Addmeal/Addmeal';
 import Order from './../Orders/Order';
 import Home from './../HomePage/HomePage';
-
-
+import Menu from '../Menu/Menu';
+import Header from '../Header';
+ import Login from '../Login/Login';
 
 
 export default class AppRouter extends React.Component{
@@ -17,13 +18,18 @@ export default class AppRouter extends React.Component{
             <>
             <Router>
 
-                <Slidebar/>
+                {/* <Slidebar/> */}
+                <Header/>
+
                 <switch>
                     <Route component={Home} path={StaticRoute} exact></Route>
                     <Route component={Home} path={`${StaticRoute}Home`} exact></Route>
                     <Route component={Restaurant} path={`${StaticRoute}Restaurant`} exact></Route>
-                    <Route component={AddMeal} path={`${StaticRoute}AddMeal`} exact></Route>
+                    <Route component={AddMeal} path={`${StaticRoute}AddMeal/:id`} exact></Route>
                     <Route component={Order} path={`${StaticRoute}Orders`} exact></Route> 
+                    <Route component={Menu} path={`${StaticRoute}Menu`} exact></Route>
+                    <Route component={Login} path={`${StaticRoute}Login`} exact></Route>
+
 
                 </switch>
             </Router>
