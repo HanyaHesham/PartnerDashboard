43f82a312a3295dedc7cfbb70144a4b2a95261cc
HanyaHesham <hanyahesham8@gmail.com>
2021-06-23 16:59:40 +0200

    Update Addmeal.js

--- src/Components/Addmeal/Addmeal.js
+++ src/Components/Addmeal/Addmeal.js
@@ -3,7 +3,7 @@ import './Addmeal.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSmile, faAngleDown, faAngleUp, faPlus, faMinus, faUtensils,faHome ,facutlery} from "@fortawesome/free-solid-svg-icons";
 
-export default class AddMreal extends React.Component{
+export default class AddMeal extends React.Component{
     state={
 
     }
@@ -16,23 +16,29 @@ export default class AddMreal extends React.Component{
        
         return (
                
-            <div class="container shadow p-3 mb-5 bg-white rounded" >
+            <div class="container shadow p-3 mb-5 bg-white rounded" style={{marginRight:100}} >
 
-                <h3>Talbat</h3>
+                <h3>FoodAway</h3>
                 <hr></hr>
                
                 <div class="row" >
                        
                      <div class="col col-md-12">
-                         {
-                           
+                         {                        
                         <form class="needs-validation" novalidate>
-                           
-                        <div class="form-group row">
-                       
+                            <div class="form-group row">
+                                
+                                <label for="validationCustom011" class="col-sm-4 col-form-label thandlabel">Image</label>
+                                <div class="col-sm-8">
+                                    <input type="file" id="validationCustom011"></input>
+                                </div>                              
+                            </div>
+                                                      
+                        <div class="form-group row">                    
                                 <label for="validationCustom01" class="col-sm-4 col-form-label thandlabel">Meal Name</label>
                                 <div class="col-sm-8">
-                                <input type="text" class="form-control inputt" id="validationCustom01"style={{width:500}}  required></input></div>
+                                    <input type="text" class="form-control inputt" id="validationCustom01"  required></input>
+                                </div>
                                     <div class="valid-feedback">
                                       Looks good!
                                      </div>
@@ -41,28 +47,23 @@ export default class AddMreal extends React.Component{
                                     </div>
                                </div>
                                
-                               <div class="form-group row">
-                                
-                                <label for="validationCustom011" class="col-sm-4 col-form-label thandlabel">Image</label>
-                                <div class="col-sm-8">
-                                <input type="file" class="form-control " id="validationCustom011"style={{width:500}}  ></input></div>
-                                   
-                               </div>
-                                <div class="form-group row">
+                            <div class="form-group row">
                                 <label for="validationCustom02" class="col-sm-4 col-form-label thandlabel">Meal Description</label>
                                 <div class="col-sm-8">
-                                <input  type="textarea" name="text" class="form-control inputtt" id="validationCustom02" style={{width:500}}  required/></div>
+                                    <textarea name="text" class="form-control inputtt" id="validationCustom02" required></textarea>
+                                </div>
                                     <div class="valid-feedback">
                                       Looks good!
                                      </div>
                                      <div class="invalid-feedback">
                                      Please add a Description.
                                     </div>
-                         </div>
+                            </div>
                          <div class="form-group row">
-                         <label for="validationCustom03" class="col-sm-4 col-form-label thandlabel">price</label>
+                         <label for="validationCustom03" class="col-sm-4 col-form-label thandlabel">Meal Price</label>
                                 <div class="col-sm-8">
-                                <input  type="number" class="form-control" id="validationCustom03" style={{width:500}}  required></input></div>
+                                    <input type="number" class="form-control" id="validationCustom03"  required></input>
+                                </div>
                                     <div class="valid-feedback">
                                       Looks good!
                                      </div>
@@ -73,22 +74,23 @@ export default class AddMreal extends React.Component{
                         
 
                          <div class="form-group row">
-                         <label for="validationCustom05" class="col-sm-3 col-form-label thandlabel">Discont</label>
-                                <div class="col-sm-3">
-                                <input  type="number" class="form-control" id="validationCustom05" style={{width:200}}  required></input></div>
+                         <label for="validationCustom05" class="col-sm-4 col-form-label thandlabel">Discount</label>
+                                <div class="col-sm-8">
+                                    <input type="number" class="form-control" id="validationCustom05" required></input>
+                                </div>
                                     <div class="valid-feedback">
                                       Looks good!
                                      </div>
                                      <div class="invalid-feedback">
                                      Please add a Starting Work.
-                                    </div>
-                                   
+                                    </div>                                  
                          </div>
                         
                          <div class="form-group row">
                          <label for="validationCustom08" class="col-sm-4 col-form-label thandlabel">Category id</label>
                                 <div class="col-sm-8">
-                                <input  type="number" class="form-control" id="validationCustom08" style={{width:500}}  required></input></div>
+                                    <input  type="number" class="form-control" id="validationCustom08" required></input>
+                                </div>
                                     <div class="valid-feedback">
                                       Looks good!
                                      </div>
@@ -97,9 +99,13 @@ export default class AddMreal extends React.Component{
                                     </div>
                          </div>
                          
-                         <input type="button"  class="btn btn-success inputtbtn" value="Save"></input>
-                        </form>
-                        
+                         <div class="form-group row justify-content-center">
+                             <div class="col-sm-8 ">
+                             <input type="button" class="btn btn-success inputtbtn" style={{width:400}} value="Save"></input>
+                             </div>
+                         </div>
+
+                        </form>                      
                     }
                        
                     </div>
