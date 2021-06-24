702284f14b936e5d438909793c0e78f3fa71b934
HanyaHesham <hanyahesham8@gmail.com>
2021-06-24 20:39:08 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faUtensils,faHome,faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {  faUtensils,faHome,faShoppingCart, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
 import './Header.css';
 export default class Header  extends React.Component{
-    state={
-
-    }
+    state={}
 
     render(){
         return(
             <nav class="navbar navbar-expand navbar-dark bg-dark" id="PartnerNav" >
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon" ></span>
+                  </button>
           
-            <div class="collapse navbar-collapse " id="navbarText" >
+            <div class="collapse navbar-collapse" id="navbarText" >
                 <ul class="navbar-nav ">
                 &nbsp;
-                <li class="nav-item active" >
+                <li class="nav-item" >
+                        <Link class="nav-link" to="/HanyaHesham/PartnerDashboard/Profile"><i class="fas">
+                                    <FontAwesomeIcon  icon={faUser} /></i> Profile  </Link>
+                </li>
+                &nbsp; &nbsp;    &nbsp; &nbsp;
+                <li class="nav-item" >
                         <Link class="nav-link" to="/HanyaHesham/PartnerDashboard/Restaurant"><i class="fas">
                                     <FontAwesomeIcon  icon={faHome} /></i> Restaurant  </Link>
                 </li>
@@ -28,11 +35,17 @@ export default class Header  extends React.Component{
                 &nbsp; &nbsp;    &nbsp; &nbsp;
                 <li class="nav-item">
                         <Link class="nav-link" to="/HanyaHesham/PartnerDashboard/Orders"><i class="fas">
-                                    <FontAwesomeIcon  icon={faShoppingCart} /></i> Order </Link>
+                                    <FontAwesomeIcon  icon={faShoppingCart} /></i> Orders </Link>
+                </li>
+
+                &nbsp; &nbsp;    &nbsp; &nbsp;
+                <li class="nav-item">
+                        <Link class="nav-link" to="/HanyaHesham/PartnerDashboard/Login"><i class="fas">
+                                    <FontAwesomeIcon  icon={faSignOutAlt} /></i> LogOut </Link>
                 </li>
                 </ul>
-  </div>
-</nav>
+            </div>
+            </nav>
         )
     }
 }
